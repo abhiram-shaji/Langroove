59a8e23bf263127bc94c1aaee12bc8b1c76e9c06
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2024-10-10 16:54:14 -0700

    Setup firebase

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,12 @@
+//firebase.ts
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import other Firebase services you want to use
+import { getAuth } from "firebase/auth"; // Example for authentication
+import { getFirestore } from "firebase/firestore"; // Example for Firestore database
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAp9-YpuTObY6H9INSKrwybQo90Azvek9E",
   authDomain: "langroove.firebaseapp.com",
@@ -13,9 +14,14 @@ const firebaseConfig = {
   storageBucket: "langroove.appspot.com",
   messagingSenderId: "259768200987",
   appId: "1:259768200987:web:c929813e23c6ed32a2edbe",
-  measurementId: "G-KCJMS7G5CE"
+  measurementId: "G-KCJMS7G5CE",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Firebase services you need, for example:
+const auth = getAuth(app); // For authentication
+const db = getFirestore(app); // For Firestore database
+
+export { auth, db }; // Export the initialized services
