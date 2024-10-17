7ad15d65a99b51f0c67830c4e8a236956629e4c8
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2024-10-16 19:13:30 -0700

    Update uselogout with react navigation logic

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -3,16 +3,18 @@
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase'; // Import Firebase auth instance
 import { Alert } from 'react-native';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
+import { RootStackParamList } from '../app/App'; // Adjust the import path as necessary
 
 export const useLogout = () => {
-  const router = useRouter();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   const handleLogout = async () => {
     try {
       // Sign out the user
       await signOut(auth);
       // Navigate to the login screen after logout
-      router.push('/LoginScreen');
+      navigation.navigate('Login'); // Use the correct screen name as defined in your navigator
     } catch (error) {
       // Handle any errors during logout
       Alert.alert('Error', 'Failed to logout. Please try again.');
