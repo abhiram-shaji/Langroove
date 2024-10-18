536840c859cb8ae184225cbe3266b65d4d1daf44
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2024-10-17 18:06:35 -0700

    Remove update avatar function

--- components/ProfileAvatar.tsx
+++ components/ProfileAvatar.tsx
@@ -1,37 +1,37 @@
 // /components/ProfileAvatar.tsx
 
 import React, { useEffect, useState } from 'react';
-import { View, TouchableOpacity } from 'react-native';
-import { Avatar, Text } from 'react-native-paper';
+import { View } from 'react-native';
+import { Avatar } from 'react-native-paper';
 import { styles } from '../styles/ProfileScreenStyles';
 import { auth, db } from '../firebase'; // Assuming these are exported from firebase.ts
-import { doc, getDoc } from 'firebase/firestore'; // Firestore methods to get data
+import { doc, getDoc } from 'firebase/firestore';
 
 const ProfileAvatar: React.FC = () => {
   const [avatarUri, setAvatarUri] = useState<string | null>(null);
 
   // Fetch user data, including the avatar URL, when the component mounts
   useEffect(() => {
-    const fetchUserAvatar = async () => {
-      try {
-        const user = auth.currentUser;
-        if (user) {
-          // Get the user's document from Firestore
-          const userDoc = await getDoc(doc(db, 'users', user.uid));
-          
-          if (userDoc.exists()) {
-            const userData = userDoc.data();
-            // Set the avatar URI from Firestore
-            setAvatarUri(userData?.avatar || 'https://robohash.org/default-avatar.png');
-          }
-        }
-      } catch (error) {
-        console.log('Error fetching user avatar:', error);
-      }
-    };
-
     fetchUserAvatar();
   }, []);
+
+  const fetchUserAvatar = async () => {
+    try {
+      const user = auth.currentUser;
+      if (user) {
+        // Get the user's document from Firestore
+        const userDoc = await getDoc(doc(db, 'users', user.uid));
+        
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          // Set the avatar URI from Firestore
+          setAvatarUri(userData?.avatar || 'https://robohash.org/default-avatar.png');
+        }
+      }
+    } catch (error) {
+      console.log('Error fetching user avatar:', error);
+    }
+  };
 
   return (
     <View style={styles.avatarContainer}>
@@ -40,12 +40,8 @@ const ProfileAvatar: React.FC = () => {
         source={{ uri: avatarUri || 'https://robohash.org/default-avatar.png' }} 
         style={styles.avatar} 
       />
-      <TouchableOpacity disabled style={styles.changeAvatarButton}>
-        <Text style={styles.changeAvatarText}>Change User Avatar</Text>
-      </TouchableOpacity>
     </View>
   );
 };
 
 export default ProfileAvatar;
-
