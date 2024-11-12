b73001cc80fc24e5d6b3f254cc01204f81cb9a84
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2024-11-11 17:43:48 -0800

    Update settings container style

--- styles/SettingsScreenStyles.ts
+++ styles/SettingsScreenStyles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    padding: 20,
+    paddingTop: 20,
     backgroundColor: colors.background,
   },
   button: {
